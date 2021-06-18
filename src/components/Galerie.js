2a1a7d49bd2d00ff754bfcbe5c621294983e71b0
Jamila <jaja_mimi@hotmail.com>
2021-06-18 18:14:01 +0200

    galerie real

--- src/components/Galerie.js
+++ src/components/Galerie.js
@@ -6,10 +6,22 @@ import { Card } from 'react-bootstrap';
 
 function Galerie(props) {
     let [illu, setIllu] = useState([
-        {nom: 'Emporium', img:'./img/realisations/emporium.png', description: "HTML/CSS/Responsive"},
-        {nom: 'Site du chef', img:'./img/realisations/sitechef.png', description: "HTML/CSS/Responsive"},
-        {nom: 'Epilogue', img:'./img/realisations/epilogue_screen.png', description: "HTML/CSS/Responsive"},
-        {nom: 'Spatial', img:'./img/realisations/spatial.png', description: "HTML/CSS/Responsive"},
+        {nom: 'Emporium', img:'./img/realisations/emporium.png', description: "HTML/CSS/Responsive", categorie: " "},
+        {nom: 'Site du chef', img:'./img/realisations/sitechef.png', description: "HTML/CSS/Responsive", categorie: " "},
+        {nom: 'Epilogue', img:'./img/realisations/epilogue_screen.png', description: "HTML/CSS/Responsive", categorie: " "},
+        {nom: 'Spatial', img:'./img/realisations/spatial.png', description: "HTML/CSS/Responsive", categorie: " "},
+        {nom: 'Affiche', img:'./img/realisations/affiche-tyn.png', description: "Affiche réalisée pour une association", categorie: " "},
+        {nom: 'Visuel pour les réseaux', img:'./img/realisations/content-reseaux.png', description: "HTML/CSS/Responsive", categorie: " "},
+        {nom: 'Flyer - TYN', img:'./img/realisations/flyertyn.png', description: "HTML/CSS/Responsive", categorie: " "},
+        {nom: 'Logo', img:'./img/realisations/logo-site.png', description: "HTML/CSS/Responsive", categorie: " "},
+        {nom: 'Visuel pour les réseaux', img:'./img/realisations/content-reseaux2.png', description: "HTML/CSS/Responsive", categorie: " "},
+        {nom: 'Logo', img:'./img/realisations/logo-tyn.png', description: "HTML/CSS/Responsive", categorie: " "},
+        {nom: 'Maquette de site', img:'./img/realisations/maquette-site.png', description: "HTML/CSS/Responsive", categorie: " "},
+        {nom: 'Carte de visite', img:'./img/realisations/mk-metal.png', description: "HTML/CSS/Responsive", categorie: " "},
+        {nom: 'Logo', img:'./img/realisations/mk.png', description: "HTML/CSS/Responsive", categorie: " "},
+        {nom: 'Maquette d application', img:'./img/realisations/read-meet.png', description: "HTML/CSS/Responsive", categorie: " "},
+        {nom: 'template photobooth', img:'./img/realisations/template.png', description: "HTML/CSS/Responsive", categorie: " "},
+        {nom: 'TFE - Application', img:'./img/realisations/tfe.png', description: "HTML/CSS/Responsive", categorie: " "},
     ])
 
     useEffect(()=> {
@@ -27,17 +39,17 @@ function Galerie(props) {
                     <div className="row">
                         {illu.map((el,i)=>{
                             return (
-                                <div className="col-md-6 mt-3" data-aos="fade-left">
+                                <div className="col-md-3 mt-3" data-aos="fade-left" data-aos-delay="100">
                                 <Card key= {i}>
-                                <div className="cadre">
-                                    <Card.Img variant="top" src={el.img} className="illu" />
-                                </div>
-                                <Card.Body>
-                                    <Card.Title>{el.nom}</Card.Title>
-                                    <Card.Text>
-                                    {el.description}
-                                    </Card.Text>
-                                </Card.Body>
+                                    <div className="cadre">
+                                        <Card.Img variant="top" src={el.img} className="illu" />
+                                    </div>
+                                    <Card.Body>
+                                        <Card.Title>{el.nom}</Card.Title>
+                                        <Card.Text>
+                                            {el.description}
+                                        </Card.Text>
+                                    </Card.Body>
                                 </Card>
                                 </div>
                             )
